d639fef4e7535ecc1ba5131fa1850629e8dfbf3f
golubD <d.golub@rj-dev.net>
2019-08-04 20:52:26 +0300

    next stranger\--

--- _webpack/src/js/index.js
+++ _webpack/src/js/index.js
@@ -15,8 +15,8 @@ app.loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
     const bunny = new PIXI.Sprite(resources.bunny.texture);
 
     // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
+    bunny.x = app.renderer.width / 3;
+    bunny.y = app.renderer.height / 3;
 
     // Rotate around the center
     bunny.anchor.x = 0.7;
